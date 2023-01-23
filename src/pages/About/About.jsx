6d43653c411dc3f0eb8eb21a744343cfb1bc8a0c
guillaume <guillaumeschmitt00@gmail.com>
2023-01-23 10:43:22 +0100

    finalisation du resposive

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -20,6 +20,7 @@ export default function About() {
     <div>
        <div className="ContainerBannerAbout">
       <Header className="HeaderAbout"/>
+      <div className="ContainerAbout">
         <img className='bannerImg' src={background} alt="image" />
         <div className="SlideAbout">
       <CollapseStyleLocation>
@@ -45,6 +46,7 @@ export default function About() {
               title="Sécurité"
               description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
         </CollapseStyleLocation> 
+        </div>
         </div>
         </div>
       <Footer img={logoFooter}/>
